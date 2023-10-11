249c488ab6c335bab6a2e6c2014ddf5ee2bdcac7
fmsaier <2661458618@qq.com>
2023-10-11 17:33:56 +0800

    .

---
+++ Code_CSharp/Test/Test/Program.cs
@@ -0,0 +1,15 @@
+﻿using /*namespace*/ System;
+
+namespace fmsaier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.Write("hello\n");
+            Console.Write("hello");
+        }
+    }
+}
+//Console.WriteLine("Hello World");
