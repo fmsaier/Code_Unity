2f812d4e84bfaf8cad143aabf972c5eefd206506
fmsaier <2661458618@qq.com>
2023-10-12 13:02:31 +0800

    .

--- Code_CSharp/Test_10.12/Test_10.12/Program.cs
+++ Code_CSharp/Test_10.12/Test_10.12/Program.cs
@@ -6,21 +6,49 @@ namespace Test_10._12
     {
         static void Main(string[] args)
         {
-            int a = Convert.ToInt32(Console.ReadLine());
-            int b = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine((a + b) / 2f);
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //int b = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine((a + b) / 2f);
 
-            a = a ^ b;
-            b = a ^ b;
-            a = a ^ b;
+            //a = a ^ b;
+            //b = a ^ b;
+            //a = a ^ b;
+            //Console.WriteLine(a);
+            //Console.WriteLine(b);
+
+            //char c = 'a';
+            //Console.WriteLine("{0} + {1} = {2}", a, b, a + b);
+            //Console.WriteLine("{0} + {1} = {2}", c, b, a + b);
+
+            //int d = 92;
+            ////bool i = a != b;
+            ////Console.WriteLine(i);
+            //char w = Convert.ToChar("k");
+            //Console.WriteLine(w);
+
+            //d = Convert.ToInt32("16");
+            //Console.WriteLine(d);
+
+            //double d = Convert.ToDouble(Console.ReadLine());
+
+            int a = Convert.ToInt32(20.1f);
             Console.WriteLine(a);
-            Console.WriteLine(b);
 
-            char c = 'a';
-            Console.WriteLine("{0} + {1} = {2}", a, b, a + b);
-            Console.WriteLine("{0} + {1} = {2}", c, b, a + b);
+            int input = 1;
+            switch (input)
+            {
+                case 0:
+                    //input = 1;
+                case 1:
+                    //input = 2;
+                case 2:
+                    //input = 3;
+                default:
+                    input = 2;
+                    break;
 
-            int d = 92;
+            }
+
 
         }
     }
