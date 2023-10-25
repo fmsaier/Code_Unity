536d0a516c6a8cd767bbd844a3a06440733c5f0b
fmsaier <2661458618@qq.com>
2023-10-25 21:44:17 +0800

    .

--- Code_CSharp/Test_10.25/Test_10.25/Program.cs
+++ Code_CSharp/Test_10.25/Test_10.25/Program.cs
@@ -1,23 +1,51 @@
-﻿namespace Test_10._25
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Test_10._25
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Print(string text)
         {
-            string str = Console.ReadLine();
-            string[] strArray = str.Split(" ");
-            int[] intArray = new int[strArray.Length]; 
-            int i = 0;
-            foreach(string number in strArray)
+            Console.WriteLine(text);
+        }
+        static void Test(int[] intArray)
+        {
+            foreach (int i in intArray)
             {
-                intArray[i] = Convert.ToInt32(strArray[i]);
-                ++i;
+                Console.WriteLine(i);
             }
-            Array.Sort(intArray);
-            foreach(int number in intArray)
+        }
+        static int Test1(int j, params int[] intArray)
+        {
+            int sum = 0;
+            foreach (int i in intArray)
             {
-                Console.WriteLine(number);
+                sum += i;
             }
+            return sum;
+        }
+        static void Main(string[] args)
+        {           
+            int[] intArray = { 1, 2, 3 };
+            Test(intArray);
+            Test(new int[] { 1, 2, });
+            Test1(6, 8, 9, 4);
+
+            //string str = Console.ReadLine();
+            //string[] strArray = str.Split(" ");
+            //int[] intArray = new int[strArray.Length]; 
+            //int i = 0;
+            //foreach(string number in strArray)
+            //{
+            //    intArray[i] = Convert.ToInt32(strArray[i]);
+            //    ++i;
+            //}
+            //Array.Sort(intArray);
+            //foreach(int number in intArray)
+            //{
+            //    Console.WriteLine(number);
+            //}
+            //Print(str);
         }
     }
 }
