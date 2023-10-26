c6841839581aafcf8d126796b1364f21d67441cd
fmsaier <2661458618@qq.com>
2023-10-26 16:37:40 +0800

    .

--- Code_CSharp/Test_10.25/Test_10.25/Program.cs
+++ Code_CSharp/Test_10.25/Test_10.25/Program.cs
@@ -4,6 +4,44 @@ namespace Test_10._25
 {
     internal class Program
     {
+        struct Stu
+        {
+            //public:
+            public int a;
+            public int b;
+
+            public int Test()
+            {
+                return 0;
+            }
+        }
+
+        enum Color
+        {
+            Red,
+            Green,
+            Black
+        };
+
+        static int F1(int n)
+        {
+            if (n == 1)
+                return 1;
+            return F1(n - 1) * n;
+        }
+        static int F2(int n)
+        {
+            if (n == 1)
+                return 1;
+            return F2(n - 1) + F1(n);
+        }
+        static int Func(int x)
+        {
+            if (x == 0 || x == 1)
+                return 1;
+            else
+                return Func(x - 1) + Func(x - 2);
+        }
         static void Print(string text)
         {
             Console.WriteLine(text);
@@ -15,7 +53,7 @@ namespace Test_10._25
                 Console.WriteLine(i);
             }
         }
-        static int Test1(int j, params int[] intArray)
+        static int Test(int j, params int[] intArray)
         {
             int sum = 0;
             foreach (int i in intArray)
@@ -24,12 +62,20 @@ namespace Test_10._25
             }
             return sum;
         }
+        //static int[] Test(int[] intArray)
+        //{
+        //    return intArray;
+        //}
+        delegate int Dele(int x);
         static void Main(string[] args)
-        {           
-            int[] intArray = { 1, 2, 3 };
-            Test(intArray);
-            Test(new int[] { 1, 2, });
-            Test1(6, 8, 9, 4);
+        {
+            //int[] intArray = { 1, 2, 3 };
+            //Test(intArray);
+            //Test(new int[] { 1, 2, 3});
+            //Test(new int[3]);
+            //Test(6, 8, 9, 4);
+
+            //Console.Write(Func(5));
 
             //string str = Console.ReadLine();
             //string[] strArray = str.Split(" ");
@@ -46,6 +92,19 @@ namespace Test_10._25
             //    Console.WriteLine(number);
             //}
             //Print(str);
+
+            Color color = Color.Red;
+            Console.WriteLine(color);
+            int number = (int)color;
+            Console.WriteLine(number);
+            Console.Write(F2(10));
+
+            //Stu s1 = { 4, 5 };
+            //s1.b = 10;
+            int[] intArray = new int[] { 5, 6, 4 };
+            //Stu[] stu = new Stu[2] { { 1, 2 }, { 5, 6 } };
+
+            Dele dele = Func;
         }
     }
 }
---
+++ Code_CSharp/Test_10.26/Test_10.26/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Test_10._26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+
+                try
+                {
+                    int n1 = Convert.ToInt32(Console.ReadLine());
+                    int n2 = Convert.ToInt32(Console.ReadLine());
+                    flag = false;
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine("have a try");
+                }
+
+            }
+
+            Custom cus = new Custom(); 
+
+        }
+    }
+}
