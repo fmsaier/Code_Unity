4f1e8e719b5b5411218b505c4104f1aceac4d8a2
fmsaier <2661458618@qq.com>
2023-10-27 22:50:07 +0800

    .

--- Code_CSharp/Test_10.26/Test_10.26/Custom.cs
+++ Code_CSharp/Test_10.26/Test_10.26/Custom.cs
@@ -8,6 +8,27 @@ namespace Test_10._26
 {
     internal class Custom
     {
+        private int age;
+        private string name;
 
+
+        public int Age
+        { 
+            get 
+            { 
+                return age; 
+            }
+            set 
+            {
+                age = value;
+            }
+        }
+
+        public string Name
+        {
+            get;set;
+        }
+
+        public bool Sex { get; set; }
     }
 }
--- Code_CSharp/Test_10.26/Test_10.26/Program.cs
+++ Code_CSharp/Test_10.26/Test_10.26/Program.cs
@@ -5,24 +5,31 @@
         static void Main(string[] args)
         {
             bool flag = true;
-            while (flag)
-            {
+            //while (flag)
+            //{
 
-                try
-                {
-                    int n1 = Convert.ToInt32(Console.ReadLine());
-                    int n2 = Convert.ToInt32(Console.ReadLine());
-                    flag = false;
-                }
-                catch (FormatException e)
-                {
-                    Console.WriteLine("have a try");
-                }
+            //    try
+            //    {
+            //        int n1 = Convert.ToInt32(Console.ReadLine());
+            //        int n2 = Convert.ToInt32(Console.ReadLine());
+            //        flag = false;
+            //    }
+            //    catch (FormatException e)
+            //    {
+            //        Console.WriteLine("have a try");
+            //    }
 
-            }
+            //}
 
-            Custom cus = new Custom(); 
+            Custom cus = new Custom();
+            cus.Age = 20;
+            Console.WriteLine(cus.Age);
 
+            cus.Name = "Test";
+            Console.WriteLine(cus.Name);
+
+            int[] intArr = new int[10] { 4,5,4,64,6,1,9,41,321,4};
+            intArr[0] = 0;
         }
     }
 }
