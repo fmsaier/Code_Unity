36fc682702b881ca42557b4ab6e75ff15a37bbb9
fmsaier <2661458618@qq.com>
2023-12-07 13:18:09 +0800

    .

--- Code_CSharp/Test_10.30/Test/Test.cs
+++ Code_CSharp/Test_10.30/Test/Test.cs
@@ -2,11 +2,17 @@
 {
     public class Test
     {
+        private int x;
         static void Swap(ref int x, ref int y)
         {
             x = x ^ y;
             y = x ^ y;
             x = x ^ y;
+        }
+        void Swap(int y)
+        {
+            x ^= y;
+            y ^= y;
         }
         static void Main(string[] args)
         {
@@ -15,6 +21,11 @@
             Console.WriteLine(a + ":" + b);
             Swap(ref a, ref b);
             Console.WriteLine(a + ":" + b);
+        }
+        void Print()
+        {
+            x++;
+            Swap(1);
         }
     }
 }
---
+++ Code_CSharp/Test_11.29/Test_11.29/Test.cs
@@ -0,0 +1,21 @@
+﻿namespace Test_11._29
+{
+    class Test
+    {
+        static int[] a = {1, 2, 3, 4, 5, 6, 7, 8};
+
+        public static void Main()
+        {
+            int i = 10;
+            object o = i;
+            o = 5;
+            Console.WriteLine(o);
+            int j = (int)o;
+            j = 6;
+            Console.WriteLine(o);
+            object obj = o;
+            obj = 6;
+            Console.WriteLine(o);
+        }
+    }
+}
